f8128017da0fec162dacc5c0d6a428675f4ad23a
Nicostaffo <staffolani.niccolo1@outlook.com>
2023-08-28 11:41:50 +0200

    commit marcogay

--- epicenergy/src/main/java/com/epicenergy/entity/User.java
+++ epicenergy/src/main/java/com/epicenergy/entity/User.java
@@ -10,7 +10,6 @@ import lombok.ToString;
 import java.util.HashSet;
 import java.util.Set;
 
-
 @Setter
 @Getter
 @NoArgsConstructor
@@ -18,24 +17,22 @@ import java.util.Set;
 @ToString
 @Entity
 @Table(name = "users", uniqueConstraints = { @UniqueConstraint(columnNames = "username"),
-		@UniqueConstraint(columnNames = "email") })
+        @UniqueConstraint(columnNames = "email") })
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+    private String lastname;
     @Column(nullable = false, unique = true)
     private String username;
     @Column(nullable = false, unique = true)
     private String email;
     @Column(nullable = false)
     private String password;
-    
+
     @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.MERGE)
-    @JoinTable(name = "users_roles",
-            joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
-            inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id")
-    )
+    @JoinTable(name = "users_roles", joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"), inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id"))
     private Set<Role> roles = new HashSet<>();
 }
--- epicenergy/src/main/java/com/epicenergy/service/AuthServiceImpl.java
+++ epicenergy/src/main/java/com/epicenergy/service/AuthServiceImpl.java
@@ -21,10 +21,6 @@ import com.epicenergy.repository.RoleRepository;
 import com.epicenergy.repository.UserRepository;
 import com.epicenergy.security.JwtTokenProvider;
 
-
-
-
-
 @Service
 public class AuthServiceImpl implements AuthService {
 
@@ -34,12 +30,11 @@ public class AuthServiceImpl implements AuthService {
     private PasswordEncoder passwordEncoder;
     private JwtTokenProvider jwtTokenProvider;
 
-
     public AuthServiceImpl(AuthenticationManager authenticationManager,
-                           UserRepository userRepository,
-                           RoleRepository roleRepository,
-                           PasswordEncoder passwordEncoder,
-                           JwtTokenProvider jwtTokenProvider) {
+            UserRepository userRepository,
+            RoleRepository roleRepository,
+            PasswordEncoder passwordEncoder,
+            JwtTokenProvider jwtTokenProvider) {
         this.authenticationManager = authenticationManager;
         this.userRepository = userRepository;
         this.roleRepository = roleRepository;
@@ -49,13 +44,11 @@ public class AuthServiceImpl implements AuthService {
 
     @Override
     public String login(LoginDto loginDto) {
-        
-    	Authentication authentication = authenticationManager.authenticate(
-        		new UsernamePasswordAuthenticationToken(
-        				loginDto.getUsername(), loginDto.getPassword()
-        		)
-        ); 
-    	
+
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        loginDto.getUsername(), loginDto.getPassword()));
+
         SecurityContextHolder.getContext().setAuthentication(authentication);
 
         String token = jwtTokenProvider.generateToken(authentication);
@@ -67,44 +60,48 @@ public class AuthServiceImpl implements AuthService {
     public String register(RegisterDto registerDto) {
 
         // add check for username exists in database
-        if(userRepository.existsByUsername(registerDto.getUsername())){
+        if (userRepository.existsByUsername(registerDto.getUsername())) {
             throw new MyAPIException(HttpStatus.BAD_REQUEST, "Username is already exists!.");
         }
 
         // add check for email exists in database
-        if(userRepository.existsByEmail(registerDto.getEmail())){
+        if (userRepository.existsByEmail(registerDto.getEmail())) {
             throw new MyAPIException(HttpStatus.BAD_REQUEST, "Email is already exists!.");
         }
 
         User user = new User();
         user.setName(registerDto.getName());
+        user.setLastname(registerDto.getLastname());
         user.setUsername(registerDto.getUsername());
         user.setEmail(registerDto.getEmail());
         user.setPassword(passwordEncoder.encode(registerDto.getPassword()));
 
         Set<Role> roles = new HashSet<>();
-        
-        if(registerDto.getRoles() != null) {
-	        registerDto.getRoles().forEach(role -> {
-	        	Role userRole = roleRepository.findByRoleName(getRole(role)).get();
-	        	roles.add(userRole);
-	        });
+
+        if (registerDto.getRoles() != null) {
+            registerDto.getRoles().forEach(role -> {
+                Role userRole = roleRepository.findByRoleName(getRole(role)).get();
+                roles.add(userRole);
+            });
         } else {
-        	Role userRole = roleRepository.findByRoleName(ERole.ROLE_USER).get();
-        	roles.add(userRole);
+            Role userRole = roleRepository.findByRoleName(ERole.ROLE_USER).get();
+            roles.add(userRole);
         }
-        
+
         user.setRoles(roles);
         System.out.println(user);
         userRepository.save(user);
 
         return "User registered successfully!.";
     }
-    
+
     public ERole getRole(String role) {
-    	if(role.equals("ADMIN")) return ERole.ROLE_ADMIN;
-    	else if(role.equals("MODERATOR")) return ERole.ROLE_MODERATOR;
-    	else return ERole.ROLE_USER;
+        if (role.equals("ADMIN"))
+            return ERole.ROLE_ADMIN;
+        else if (role.equals("MODERATOR"))
+            return ERole.ROLE_MODERATOR;
+        else
+            return ERole.ROLE_USER;
     }
-    
+
 }
